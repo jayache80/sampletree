3820a2e16b3199f6a632e8267828487d6cb0699b
jim jammer <15721514+jayache80@users.noreply.github.com>
2023-05-27 02:29:27 -0700

    Program keys

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,45 @@ use termion::input::TermRead;
 use termion::raw::{IntoRawMode, RawTerminal};
 
 fn play(path: &PathBuf, stream_handle: &OutputStreamHandle, stdout: &mut RawTerminal<Stdout>, idx: &String) {
+    let mut z = " ";
+    let mut x = " ";
+    let mut c = " ";
+    let mut v = " ";
+    let mut idx_ = String::from("");
+
+    match idx.as_str() {
+        "z" => {
+            z = "x";
+        },
+        "x" => {
+            x = "x";
+        },
+        "c" => {
+            c = "x";
+        },
+        "v" => {
+            v = "x";
+        },
+        _ => {
+            idx_ = format!("{} ", idx).to_string();
+        },
+    }
+
     write!(
         stdout,
-        "\r{}{} {}",
+        "\r{}{}z[{}] x[{}] c[{}] v[{}]\r\n{}{}{}",
+        termion::cursor::Up(1),
         termion::clear::CurrentLine,
-        idx,
+        z,
+        x,
+        c,
+        v,
+        termion::clear::CurrentLine,
+        idx_,
         path.display().to_string(),
         ).unwrap();
     stdout.lock().flush().unwrap();
+
     if let Ok(sound_file) = File::open(path.display().to_string()) {
         if let Ok(source) = rodio::Decoder::new(BufReader::new(sound_file)) {
             stream_handle.play_raw(source.convert_samples()).ok();
@@ -31,16 +62,12 @@ fn make_idx(i: usize, max: usize) -> String {
 }
 
 fn walk_and_play(path: &String) {
-    // Audio backend
-    let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
-
-    // Set terminal to raw mode to allow reading stdin one key at a time
-    let mut stdout = io::stdout().into_raw_mode().unwrap();
-
-    // Use asynchronous stdin
-    let mut stdin = termion::async_stdin().keys();
-
+    let mut i = 0;
     let mut paths = Vec::new();
+    let mut z: Option<&PathBuf> = None;
+    let mut x: Option<&PathBuf> = None;
+    let mut c: Option<&PathBuf> = None;
+    let mut v: Option<&PathBuf> = None;
 
     for file in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
         if file.metadata().unwrap().is_file() {
@@ -49,10 +76,18 @@ fn walk_and_play(path: &String) {
         }
     }
 
-    let mut i = 0;
     if paths.len() == 0 {
         return;
     }
+
+    // Audio backend
+    let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
+
+    // Set terminal to raw mode to allow reading stdin one key at a time
+    let mut stdout = io::stdout().into_raw_mode().unwrap();
+
+    // Use asynchronous stdin
+    let mut stdin = termion::async_stdin().keys();
 
     play(&paths[i], &stream_handle, &mut stdout, &make_idx(i, paths.len()));
 
@@ -76,6 +111,46 @@ fn walk_and_play(path: &String) {
                     }
                     play(&paths[i], &stream_handle, &mut stdout, &make_idx(i, paths.len()));
                 },
+                termion::event::Key::Char('z') => {
+                    if z == None {
+                        z = Some(&paths[i]);
+                    }
+                    play(z.unwrap(), &stream_handle, &mut stdout, &"z".to_string());
+                },
+                termion::event::Key::Char('Z') => {
+                    z = Some(&paths[i]);
+                    play(z.unwrap(), &stream_handle, &mut stdout, &"z".to_string());
+                },
+                termion::event::Key::Char('x') => {
+                    if x == None {
+                        x = Some(&paths[i]);
+                    }
+                    play(x.unwrap(), &stream_handle, &mut stdout, &"x".to_string());
+                },
+                termion::event::Key::Char('X') => {
+                    x = Some(&paths[i]);
+                    play(x.unwrap(), &stream_handle, &mut stdout, &"x".to_string());
+                },
+                termion::event::Key::Char('c') => {
+                    if c == None {
+                        c = Some(&paths[i]);
+                    }
+                    play(c.unwrap(), &stream_handle, &mut stdout, &"c".to_string());
+                },
+                termion::event::Key::Char('C') => {
+                    c = Some(&paths[i]);
+                    play(c.unwrap(), &stream_handle, &mut stdout, &"c".to_string());
+                },
+                termion::event::Key::Char('v') => {
+                    if v == None {
+                        v = Some(&paths[i]);
+                    }
+                    play(v.unwrap(), &stream_handle, &mut stdout, &"v".to_string());
+                },
+                termion::event::Key::Char('V') => {
+                    v = Some(&paths[i]);
+                    play(v.unwrap(), &stream_handle, &mut stdout, &"v".to_string());
+                },
                 termion::event::Key::Char('q') => {
                     break 'outer;
                 },
@@ -85,7 +160,7 @@ fn walk_and_play(path: &String) {
                 }
             }
         }
-        thread::sleep(time::Duration::from_millis(1));
+        thread::sleep(time::Duration::from_micros(1000));
     }
     write!(stdout, "\r\n").unwrap();
 }
@@ -97,7 +172,7 @@ fn main() {
         println!("Provide path to directory to traverse as first argument\n");
     } else {
         let path = &args[1];
-        println!("Traversing {}", path);
+        println!("Traversing {}\n", path);
         walk_and_play(path);
     }
 }
